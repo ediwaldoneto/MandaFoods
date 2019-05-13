98e988f26518ef3a93b52bb4506374cb86d93a71
Neto <ediwaldo.neto@outlook.com>
2019-05-13 08:19:51 -0300

    Implementação produto empresa

--- app/src/main/java/com/br/nt/mandafoods/activity/NovoProdutoEmpresaActivity.java
+++ app/src/main/java/com/br/nt/mandafoods/activity/NovoProdutoEmpresaActivity.java
@@ -3,19 +3,78 @@ package com.br.nt.mandafoods.activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import com.br.nt.mandafoods.R;
+import com.br.nt.mandafoods.helper.UsuarioFirebase;
+import com.br.nt.mandafoods.model.Produto;
 
 public class NovoProdutoEmpresaActivity extends AppCompatActivity {
+
+    private EditText editProdutoNome, editProdutoDescricao,
+            editProdutoPreco;
+    private String usuarioLogado;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_novo_produto_empresa);
 
+        inicializarComponentes();
+        usuarioLogado = UsuarioFirebase.getIdUsuario();
+
+
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Novo Produto");
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
     }
+
+    public void validarDadosProduto(View view) {
+
+        //Valida se os campos foram preenchidos
+        String nome = editProdutoNome.getText().toString();
+        String descricao = editProdutoDescricao.getText().toString();
+        String preco = editProdutoPreco.getText().toString();
+
+        if (!nome.isEmpty()) {
+            if (!descricao.isEmpty()) {
+                if (!preco.isEmpty()) {
+
+                    Produto produto = new Produto();
+                    produto.setIdUsuario(usuarioLogado);
+                    produto.setNome(nome);
+                    produto.setDescricao(descricao);
+                    produto.setPreco(Double.parseDouble(preco));
+                    produto.salvar();
+
+                    finish();
+                    exibirMensagem("Produto salvo com sucesso!");
+
+                } else {
+                    exibirMensagem("Digite um preço para o produto");
+                }
+            } else {
+                exibirMensagem("Digite uma descrição para o produto");
+            }
+        } else {
+            exibirMensagem("Digite um nome para o produto");
+        }
+
+    }
+
+    private void exibirMensagem(String texto) {
+        Toast.makeText(this, texto, Toast.LENGTH_SHORT)
+                .show();
+    }
+
+    private void inicializarComponentes(){
+        editProdutoDescricao = findViewById(R.id.editProdutoDescricao);
+        editProdutoNome = findViewById(R.id.editProdutoNome);
+        editProdutoPreco = findViewById(R.id.editProdutoPreco);
+    }
+
 }
