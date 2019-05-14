15b40a7a35f4eca16ab473cf3f80fb7bcd168191
Neto <ediwaldo.neto@outlook.com>
2019-05-14 13:20:24 -0300

    Evolucao tela de empresas

--- app/src/main/java/com/br/nt/mandafoods/activity/HomeActivity.java
+++ app/src/main/java/com/br/nt/mandafoods/activity/HomeActivity.java
@@ -4,6 +4,8 @@ package com.br.nt.mandafoods.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -11,26 +13,65 @@ import android.view.MenuItem;
 
 
 import com.br.nt.mandafoods.R;
+import com.br.nt.mandafoods.adapter.AdapterEmpresa;
 import com.br.nt.mandafoods.helper.ConfiguracaoFirebase;
+import com.br.nt.mandafoods.model.Empresa;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 import com.miguelcatalan.materialsearchview.MaterialSearchView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HomeActivity extends AppCompatActivity {
 
     private FirebaseAuth autenticacao;
     private MaterialSearchView searchView;
+    private RecyclerView recyclerEmpresa;
+    private List<Empresa> empresas = new ArrayList<>();
+    private DatabaseReference firebaseRef;
+    private AdapterEmpresa adapterEmpresa;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
+        firebaseRef = ConfiguracaoFirebase.getFirebase();
         autenticacao = ConfiguracaoFirebase.getFirebaseAutenticacao();
         iniciarlizaComponentes();
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         toolbar.setTitle("Manda Foods");
         setSupportActionBar(toolbar);
+
+        //Configura recyclerview
+        recyclerEmpresa.setLayoutManager(new LinearLayoutManager(this));
+        recyclerEmpresa.setHasFixedSize(true);
+        adapterEmpresa = new AdapterEmpresa(empresas);
+        recyclerEmpresa.setAdapter( adapterEmpresa );
+
+        //Recuperando dados das empresas
+        recuperarEmpresas();
+
+        //Configuração do search view
+        searchView.setHint("Pesquisar restaurantes");
+        searchView.setOnQueryTextListener(new MaterialSearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String newText) {
+                pesquisarEmpresas( newText );
+                return true;
+            }
+        });
 
     }
 
@@ -82,6 +123,61 @@ public class HomeActivity extends AppCompatActivity {
 
     private void iniciarlizaComponentes(){
         searchView = findViewById(R.id.materialSearchView);
+        recyclerEmpresa = findViewById(R.id.recyclerEmpresa);
+    }
+
+    private void recuperarEmpresas(){
+
+        DatabaseReference empresaRef = firebaseRef.child("empresas");
+        empresaRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                empresas.clear();
+
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    empresas.add( ds.getValue(Empresa.class) );
+                }
+
+                adapterEmpresa.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+    }
+
+    private void pesquisarEmpresas(String pesquisa){
+
+        DatabaseReference empresasRef = firebaseRef
+                .child("empresas");
+        Query query = empresasRef.orderByChild("nome")
+                .startAt(pesquisa)
+                .endAt(pesquisa + "\uf8ff" );
+
+        query.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+
+                empresas.clear();
+
+                for (DataSnapshot ds: dataSnapshot.getChildren()){
+                    empresas.add( ds.getValue(Empresa.class) );
+                }
+
+                adapterEmpresa.notifyDataSetChanged();
+
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 
 
